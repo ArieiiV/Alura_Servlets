635acafc597d58973401fc3cf814ed73bdd12e8a
Karoline Vieira <kvianavieira@hotmail.com>
2020-03-31 11:24:44 -0300

    Listando empresas com servlets

---
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+	private static List<Empresa> lista = new ArrayList<Empresa>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Alura");
+		
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		
+		lista.add(empresa);
+		lista.add(empresa2);
+		
+	}
+
+	public void adiciona(Empresa empresa) {
+		Banco.lista.add(empresa);
+		System.out.println(empresa);
+
+	}
+	
+	public List<Empresa> getemEmpresas(){
+		return Banco.lista;
+	}
+
+}
